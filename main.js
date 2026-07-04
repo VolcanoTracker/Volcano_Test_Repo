9a8bf1210aec0682fc29537421f69e633d34af31
VolcanoTracker <jeremiahhelg@gmail.com>
2026-07-04 13:10:19 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,27 +8,12 @@ fetch('https://corsproxy.io/?url=https://volcanoes.usgs.gov/vsc/api/volcanoApi/v
     .then(r => r.json())
     .then(data => {
         data.forEach(v => {
-            if (v.latitude && v.longitude) {
-                // Check for variations in naming to ensure we get the right data
-                const name = v.volcano_name || v.vName || v.name || "Unnamed Volcano";
-                const alert = (v.alert_level || v.color_code || v.alertLevel || "").toUpperCase();
-                
-                let color = 'green'; // Default
-                if (alert.includes('RED')) color = 'red';
-                else if (alert.includes('ORANGE')) color = 'orange';
-                else if (alert.includes('YELLOW')) color = 'yellow';
-                else if (alert.includes('GREEN') || alert.includes('NORMAL')) color = 'green';
-                else if (alert.includes('UNASSIGNED')) color = 'blue';
-
-                const customIcon = new L.Icon({
-                    iconUrl: `https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
-                    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-                    iconSize: [25, 41],
-                    iconAnchor: [12, 41],
-                    popupAnchor: [1, -34],
-                    shadowSize: [41, 41]
-                });
-
+    // This will print the actual list of keys for every single volcano object
+    console.log("Actual keys present:", Object.keys(v));
+    
+    // We stop the loop immediately so we only get one log
+    throw new Error("Debugging: Check console for keys!");
+});
                 L.marker([v.latitude, v.longitude], { icon: customIcon })
                  .addTo(map)
                  .bindPopup(`<b>${name}</b><br>Status: ${alert || 'None Provided'}`);
